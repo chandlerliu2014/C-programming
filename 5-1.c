ab8b5a57ef1c3ba22ce700c698f30cd5d75a0721
root <root@localhost.localdomain>
2014-10-25 20:16:37 -0700

    add 5-1.c

---
+++ 5-1.c
@@ -0,0 +1,29 @@
+
+
+int getint(int *pn)
+{
+	int c,d,sign;
+
+	while(isspace(c = getch()))
+		;
+	if(!isdigit(c) && c != EOF && c != '+' && c != '-') {
+		ungetch(c);
+		return 0;
+	}
+	sign = (c == '-') ? -1 : 1;
+	if(c == '+' || c == '-') {
+		d = c;
+		if(!isdigit(c = getch())) {
+			if(c != EOF)
+				ungetch(c);
+			ungetch(d);
+			return d;
+		}
+	}
+	for(*pn = 0;isdigit(c);c = getch())
+		*pn = 10 * *pn + (c - '0');
+	*pn *= sign;
+	if(c != EOF)
+		ungetch(c);
+	return c;
+}
